da86ce1d55950bcec803d38a1ad9c078d86674ca
Anton Sheremet <a.sheremet@besmart-mobile.com>
2018-06-16 10:02:08 +0300

    removed unnecessary file and updated var1.swift

--- var1.swift
+++ var1.swift
@@ -1,1 +1,25 @@
-print ("Hello World")
+var matrix = [[1, 2, 3], [2, 4, -1], [5, 2, 0]]
+let matrixLength = matrix.count - 1
+var currentLength = 0
+var tmpArray = [Int]()
+var sumCurrent = 0
+var sumPrevious = 0
+for _ in 0...matrixLength {
+	for i in 0...matrixLength {
+		currentLength = matrix[i].count - 1
+		sumCurrent = 0;
+		for j in 0...currentLength {
+			sumCurrent += matrix[i][j]
+		}
+		if i > 0 && sumCurrent < sumPrevious {
+			tmpArray = matrix[i]
+			matrix[i] = matrix[i - 1]
+			matrix[i - 1] = tmpArray
+		}
+		sumPrevious = sumCurrent
+	}
+}
+
+for i in 0...matrixLength {
+	print(matrix[i])
+}
